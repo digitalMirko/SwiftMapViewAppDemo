f7a6710088c0ca3a32d6ef74705a6db15c086624
Mirko Cukich <mirkocukich@gmail.com>
2019-10-07 23:19:42 -0500

    Map set to zoom in on Willis Tower (Chicago, IL) when it first starts up

--- Mao View App/ViewController.swift
+++ Mao View App/ViewController.swift
@@ -13,26 +13,43 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    // Coordinates - Willis Tower in Chicago, IL
+    // 233 South Wacker Drive, Chicago, IL 60606, United States of America
+    var latitude = 41.878872
+    var longitude = -87.635909
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // when mapview loads up - Willis Tower
+        // span of how far we zoomed in on map
+        let span = MKCoordinateSpan.init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        // Willis Tower area
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: latitude, longitude: longitude), span: span)
+        mapView.setRegion(region, animated: true)
+        
         
         
     }
     
     @IBAction func standardBtn(_ sender: Any) {
         
+        mapView.mapType = MKMapType.standard
+        
         
     }
     
     @IBAction func satelliteBtn(_ sender: Any) {
         
+        mapView.mapType = MKMapType.satellite
         
     }
     
     
     @IBAction func hybridBtn(_ sender: Any) {
         
+        mapView.mapType = MKMapType.hybrid
     }
     
     
